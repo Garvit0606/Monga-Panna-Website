5d3f54c8ce03812b10faddd23b98125152d27292
Garvit0606 <workofgarvit@gmail.com>
2026-02-02 09:21:08 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,16 +1,25 @@
-
 function addProduct() {
   let name = document.getElementById("name").value;
   let price = document.getElementById("price").value;
   let size = document.getElementById("size").value;
-  let imgInput = document.getElementById("image");
+  let file = document.getElementById("image").files[0];
+
+  if (!name || !price || !file) {
+    alert("Please fill all fields");
+    return;
+  }
 
   let reader = new FileReader();
-  reader.onload = function() {
+  reader.onload = function () {
     let products = JSON.parse(localStorage.getItem("products")) || [];
-    products.push({ name, price, size, image: reader.result });
+    products.push({
+      name: name,
+      price: price,
+      size: size,
+      image: reader.result
+    });
     localStorage.setItem("products", JSON.stringify(products));
     alert("Product Added Successfully");
   };
-  reader.readAsDataURL(imgInput.files[0]);
+  reader.readAsDataURL(file);
 }
